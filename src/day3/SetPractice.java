d4462fa4e35cbddd61131a553bbb60f543a9ddb7
Oleg Iakim <execute067@gmail.com>
2021-07-29 11:47:32 -0700

    Set is cool can't wait to learn

--- src/day3/SetPractice.java
+++ src/day3/SetPractice.java
@@ -4,5 +4,6 @@ public class SetPractice {
 
     // Set is a collection type that only store unique item
     // if you add dublicate it will just remove it
+    // it's a collection of unique elements
 
 }
