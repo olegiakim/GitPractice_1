9716938d63cad13716dd00b462e7966e72c07532
Oleg Iakim <execute067@gmail.com>
2021-07-29 13:25:28 -0700

    master added stuff

--- src/day3/Movie.java
+++ src/day3/Movie.java
@@ -11,4 +11,6 @@ public class Movie {
     //the spouse came in and watched the movie until 20 mins
     // watching 10 more mins
     // watching until 25 mins
+
+    // this is stuff from master
 }
