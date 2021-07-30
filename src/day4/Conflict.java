9149a454e6ad8fec7cd5e0684eaae38dd8de3860
Oleg Iakim <execute067@gmail.com>
2021-07-30 07:51:02 -0700

    final commit

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -6,4 +6,6 @@ public class Conflict {
     // This is collection branch content
 
     //Approved
+
+    //Add a different comment in the class `Conflict` with `This is the change collection2 branch does not know about
 }
